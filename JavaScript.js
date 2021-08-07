56cd914881fdef37fd5866dca51143de681446ec
Karlsson777 <88598986+Karlsson777@users.noreply.github.com>
2021-08-08 01:10:16 +0300

    new shit

---
+++ JavaScript.js
@@ -0,0 +1,6 @@
+﻿var HB = document.querySelector(".header__boorger");
+var HM = document.querySelector(".header__menu");
+HB.addEventListener("click", function () {
+    HB.classList.toggle("active");
+    HM.classList.toggle("active");
+});
